2d49973ac3101ff4c0eefd91bccab1c751b83601
Zac <zacaree@gmail.com>
2018-05-16 13:53:33 -0500

    Hustler Story page draft 1 finished

--- src/components/Page-About-HustlerStory.jsx
+++ src/components/Page-About-HustlerStory.jsx
@@ -8,14 +8,16 @@ const HustlerStory = () => (
       <h1>Hustler's Story</h1>
       <div className="accentBar" />
 
-      <div className="ctr-left">
-        <h2>We make the best…</h2>
-        <h2>then, we make it better</h2>
-        <p>Hustler’s commitment to purposeful innovation gives you everything you could ask for in a mower, and so much more. </p>
-        <p>In 1964, we introduced the Hustler, the world’s first zero-turn mower. The Hustler allowed our customers to tackle their lawns in record time, with unparalleled precision. This easy-to-handle, highly maneuverable innovation revolutionized the mower industry, and we’ve been committed to making innovative outdoor power equipment ever since.</p>
-      </div>
-      <div className="ctr-right">
-        <img src={HistoryImg} alt="" className="aspectRatio" />
+      <div className="row-offset-1rem">
+        <div className="ctr-left">
+          <h2>We make the best…</h2>
+          <h2>then, we make it better</h2>
+          <p className="bodyCopy">Hustler’s commitment to purposeful innovation gives you everything you could ask for in a mower, and so much more. </p>
+          <p className="bodyCopy">In 1964, we introduced the Hustler, the world’s first zero-turn mower. The Hustler allowed our customers to tackle their lawns in record time, with unparalleled precision. This easy-to-handle, highly maneuverable innovation revolutionized the mower industry, and we’ve been committed to making innovative outdoor power equipment ever since.</p>
+        </div>
+        <div className="ctr-right">
+          <img src={HistoryImg} alt="" className="" />
+        </div>
       </div>
     </div>
   </div>
