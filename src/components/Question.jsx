1c3cdf4c9bddbf51901ef6bdec2d9de0574b5a8e
Zac <zacaree@gmail.com>
2018-05-18 14:54:08 -0500

    Faq page checkpoint

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,17 +1,56 @@
-import React from 'react';
+/* eslint react/prop-types: 0 */
+
+import React, { Component } from 'react';
 import DropdownArrow from './../img/DropdownArrow';
 
-const Question = props => (
-  <div className="card" >
-    <div className="ctr-question">
-      <h2>Question</h2>
-      <p>{props.faq.question}</p>
-    </div>
-    <div className="txtBtn">
-      <h3>Answer</h3>
-      <DropdownArrow />
-    </div>
-  </div>
-);
+export default class Question extends Component {
+  state = {
+    showDropdown: false,
+  };
 
-export default Question;
+
+  grabHeight = React.createRef();
+
+
+  toggleDropdown = () => {
+    // this.props.clickHandler();
+    this.setState({
+      showDropdown: !this.state.showDropdown,
+      height: this.grabHeight.current.clientHeight,
+    });
+  }
+
+
+  render() {
+    const { question, answer } = this.props.faq;
+    const { showDropdown, height } = this.state;
+    const currentHeight = showDropdown ? height : 0;
+    const itemZIndex = 90 - this.props.i;
+    const accentVisible = this.state.showDropdown ? 1 : 0;
+
+    return (
+      <div className="card" style={{ zIndex: itemZIndex }}>
+        <div className="accent" style={{ opacity: accentVisible }} />
+        <div className="ctr-question">
+          <h2>Question</h2>
+          <p>{question}</p>
+        </div>
+        <div className="txtBtn" onClick={this.toggleDropdown} >
+          <h3>Answer</h3>
+          <DropdownArrow />
+        </div>
+        <div className="ctr-dropdown">
+          <div className="dropdown" style={{ height: `${currentHeight}px` }} >
+
+            <div className="ctr-models" ref={this.grabHeight}>
+              <p>{answer}</p>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+// export default Question;
